8f8328cdcdd4d7a95d6ce729026608470d26ad6b
Priit Pärkson <priit.parkson@cyber.ee>
2018-09-18 10:33:08 +0300

    Removed some sharemind configurations, these are now created by build-sdk and kept in their respective repositories.

--- sharemind-sdk/scripts/user/sdk.sh
+++ sharemind-sdk/scripts/user/sdk.sh
@@ -16,3 +16,7 @@ cp --recursive --no-preserve=mode,ownership,timestamps "${SDK_DESKTOP_ICONS_PATH
 # Install Sharemind configuration
 mkdir --parents ~/.config
 cp --recursive --no-preserve=mode,ownership,timestamps "${SDK_SM_CONFIG_PATH}/." ~/.config/
+# Install Sharemind configurations created by build-sdk
+cp /usr/local/sharemind/bin/*.cfg ~/.config/sharemind
+# Expand ModelEvaluatorConfiguration configs to full paths
+sed -i "s+ModelEvaluatorConfiguration\s\=\s+ModelEvaluatorConfiguration = $(readlink -f ~/.config/sharemind)/+" ~/.config/sharemind/*
