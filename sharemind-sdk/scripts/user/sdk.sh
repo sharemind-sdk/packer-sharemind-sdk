cf1fe596cf45e6bd2a4dd589321d1aa6c017892d
Priit Pärkson <priit.parkson@cyber.ee>
2019-03-14 14:06:20 +0200

    Update mod_shared3p_emu configuration location

--- sharemind-sdk/scripts/user/sdk.sh
+++ sharemind-sdk/scripts/user/sdk.sh
@@ -18,5 +18,7 @@ mkdir --parents ~/.config
 cp --recursive --no-preserve=mode,ownership,timestamps "${SDK_SM_CONFIG_PATH}/." ~/.config/
 # Install Sharemind configurations created by build-sdk
 cp /usr/local/sharemind/bin/*.cfg ~/.config/sharemind
+# Install shared3p_emu configuration files
+cp /usr/local/src/sharemind-sdk.git/mod_shared3p_emu/packaging/configs/sharemind/*.conf ~/.config/sharemind
 # Expand ModelEvaluatorConfiguration configs to full paths
 sed -i "s+ModelEvaluatorConfiguration\s\=\s+ModelEvaluatorConfiguration = $(readlink -f ~/.config/sharemind)/+" ~/.config/sharemind/*
