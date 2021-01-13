9de4e6ee3e71ebd5284576f82df8ec94c111e164
Riivo Talviste <riivo.talviste@cyber.ee>
2021-01-13 14:45:55 +0200

    gksu is no longer available, so remove the configuration editing.

--- sharemind-sdk/scripts/root/desktop-environment.sh
+++ sharemind-sdk/scripts/root/desktop-environment.sh
@@ -12,10 +12,6 @@ apt-get install --yes lxde
 sed -i "s/^#autologin-user=$/autologin-user=${DE_AUTOLOGIN_USERNAME}/" /etc/lightdm/lightdm.conf
 sed -i "s/^#autologin-user-timeout=0$/autologin-user-timeout=0/" /etc/lightdm/lightdm.conf
 
-# /etc/alternatives/libgksu-gconf-defaults
-sed -i 's#/apps/gksu/sudo-mode false#/apps/gksu/sudo-mode true#' /etc/alternatives/libgksu-gconf-defaults
-update-gconf-defaults
-
 # /etc/xdg/pcmanfm/LXDE/pcmanfm.conf
 sed -i "s/^wallpaper_mode=.*$/wallpaper_mode=${DE_DESKTOP_BG_MODE}/" /etc/xdg/pcmanfm/LXDE/pcmanfm.conf
 sed -i "s/^desktop_bg=.*$/desktop_bg=${DE_DESKTOP_BG_COLOR}/" /etc/xdg/pcmanfm/LXDE/pcmanfm.conf
